59ff9304c999761006213fb3c0a4af6d5df4b723
Jérôme Zecca <jerome.zecca@cpiglobal.com>
2019-06-24 14:54:58 +0200

    Adjustment per channel (RGB, R, G or B).

--- test.js
+++ test.js
@@ -30,11 +30,35 @@ describe('curveAdjust', () => {
         { x: 255, y: 255 },
     ];
 
-    test('hex', () => {
-        expect(curveAdjust('#5a2149', curve)).toBe('#9d6b8d');
-        expect(curveAdjust('#040700', curve)).toBe('#585955');
-        expect(curveAdjust('#ead184', curve)).toBe('#f5e9c2');
-        expect(curveAdjust('#9c6d02', curve)).toBe('#d2af56');
-        expect(curveAdjust('#6e921b', curve)).toBe('#b0cc67');
+    test('rgb', () => {
+        expect(curveAdjust.rgb('#5a2149', curve)).toBe('#9d6b8d');
+        expect(curveAdjust.rgb('#040700', curve)).toBe('#585955');
+        expect(curveAdjust.rgb('#ead184', curve)).toBe('#f5e9c2');
+        expect(curveAdjust.rgb('#9c6d02', curve)).toBe('#d2af56');
+        expect(curveAdjust.rgb('#6e921b', curve)).toBe('#b0cc67');
+    });
+
+    test('red', () => {
+        expect(curveAdjust.red('#5a2149', curve)).toBe('#9d2149');
+        expect(curveAdjust.red('#040700', curve)).toBe('#580700');
+        expect(curveAdjust.red('#ead184', curve)).toBe('#f5d184');
+        expect(curveAdjust.red('#9c6d02', curve)).toBe('#d26d02');
+        expect(curveAdjust.red('#6e921b', curve)).toBe('#b0921b');
+    });
+
+    test('green', () => {
+        expect(curveAdjust.green('#5a2149', curve)).toBe('#5a6b49');
+        expect(curveAdjust.green('#040700', curve)).toBe('#045900');
+        expect(curveAdjust.green('#ead184', curve)).toBe('#eae984');
+        expect(curveAdjust.green('#9c6d02', curve)).toBe('#9caf02');
+        expect(curveAdjust.green('#6e921b', curve)).toBe('#6ecc1b');
+    });
+
+    test('blue', () => {
+        expect(curveAdjust.blue('#5a2149', curve)).toBe('#5a218d');
+        expect(curveAdjust.blue('#040700', curve)).toBe('#040755');
+        expect(curveAdjust.blue('#ead184', curve)).toBe('#ead1c2');
+        expect(curveAdjust.blue('#9c6d02', curve)).toBe('#9c6d56');
+        expect(curveAdjust.blue('#6e921b', curve)).toBe('#6e9267');
     });
 });
